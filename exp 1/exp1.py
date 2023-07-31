49e094384b11975f64e6902ba653c71ab3fa2413
adityaparikh7 <asparikh03@gmail.com>
2023-07-31 12:18:06 +0530

    update

---
+++ exp 1/exp1.py
@@ -0,0 +1,88 @@
+# import numpy as np
+
+# def threshold(x, threshold_value=0):
+#     return 1 if x >= threshold_value else 0
+
+# def sigmoid(x):
+#     return 1 / (1 + np.exp(-x))
+
+# def relu(x):
+#     return max(0, x)
+
+# def main():
+#     print("Choose an activation function:")
+#     print("1. Threshold")
+#     print("2. Sigmoid")
+#     print("3. ReLU")
+
+#     choice = int(input("Enter the number corresponding to the activation function: "))
+
+#     if choice == 1:
+#         threshold_value = float(input("Enter the threshold value: "))
+#         x = float(input("Enter the input value: "))
+#         result = threshold(x, threshold_value)
+#     elif choice == 2:
+#         x = float(input("Enter the input value: "))
+#         result = sigmoid(x)
+#     elif choice == 3:
+#         x = float(input("Enter the input value: "))
+#         result = relu(x)
+#     else:
+#         print("Invalid choice.")
+#         return
+
+#     print("Result:", result)
+
+# if __name__ == "__main__":
+#     main()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def threshold(x, threshold_value=0):
+    return 1 if x >= threshold_value else 0
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return max(0, x)
+
+def plot_activation_function(func, func_name):
+    x = np.linspace(-5, 5, 100)
+    y = [func(i) for i in x]
+
+    plt.plot(x, y)
+    plt.title(func_name + " Activation Function")
+    plt.xlabel("Input")
+    plt.ylabel("Output")
+    plt.grid(True)
+    plt.show()
+
+def main():
+    print("Choose an activation function:")
+    print("1. Threshold")
+    print("2. Sigmoid")
+    print("3. ReLU")
+
+    choice = int(input("Enter the number corresponding to the activation function: "))
+
+    if choice == 1:
+        threshold_value = float(input("Enter the threshold value: "))
+        func = lambda x: threshold(x, threshold_value)
+        func_name = "Threshold"
+    elif choice == 2:
+        func = sigmoid
+        func_name = "Sigmoid"
+    elif choice == 3:
+        func = relu
+        func_name = "ReLU"
+    else:
+        print("Invalid choice.")
+        return
+
+    plot_activation_function(func, func_name)
+
+if __name__ == "__main__":
+    main()
---
+++ exp 1/exp1kashvi.py
@@ -0,0 +1,124 @@
+# import numpy as np
+
+# def identity(x):
+#     return x
+
+# def binary_step(x):
+#     return np.where(x < 0, 0, 1)
+
+# def sigmoid(x):
+#     return 1 / (1 + np.exp(-x))
+
+# def bipolar_sigmoid(x):
+#     return (2 / (1 + np.exp(-x))) - 1
+
+# def relu(x):
+#     return np.maximum(0, x)
+
+# # Function to get user input
+# def get_user_input():
+#     try:
+#         input_str = input("Enter input values separated by spaces: ")
+#         input_values = [float(val) for val in input_str.split()]
+#         return input_values
+#     except ValueError:
+#         print("Invalid input! Please enter valid numbers separated by spaces.")
+#         return get_user_input()
+
+# # Test the activation functions with user input
+# if __name__ == "__main__":
+#     user_input = get_user_input()
+#     x = np.array(user_input)
+
+#     print("Identity function:")
+#     print(identity(x))
+
+#     print("Binary Step function:")
+#     binary_step_output = binary_step(x)
+#     print(binary_step_output)
+
+#     print("Sigmoid function:")
+#     print(sigmoid(x))
+
+#     print("Bipolar Sigmoid function:")
+#     print(bipolar_sigmoid(x))
+
+#     print("ReLU function:")
+#     print(relu(x))
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def identity(x):
+    return x
+
+def binary_step(x):
+    return np.where(x < 0, 0, 1)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def bipolar_sigmoid(x):
+    return (2 / (1 + np.exp(-x))) - 1
+
+def relu(x):
+    return np.maximum(0, x)
+
+# Function to get user input
+def get_user_input():
+    try:
+        input_str = input("Enter input values separated by spaces: ")
+        input_values = [float(val) for val in input_str.split()]
+        return input_values
+    except ValueError:
+        print("Invalid input! Please enter valid numbers separated by spaces.")
+        return get_user_input()
+
+# Test the activation functions with user input
+if __name__ == "__main__":
+    user_input = get_user_input()
+    x = np.array(user_input)
+
+    print("Identity function:")
+    print(identity(x))
+
+    print("Binary Step function:")
+    binary_step_output = binary_step(x)
+    print(binary_step_output)
+
+    print("Sigmoid function:")
+    print(sigmoid(x))
+
+    print("Bipolar Sigmoid function:")
+    print(bipolar_sigmoid(x))
+
+    print("ReLU function:")
+    print(relu(x))
+
+    # Plotting the activation functions
+    plt.figure(figsize=(12, 8))
+
+    plt.subplot(2, 2, 1)
+    plt.plot(x, identity(x))
+    plt.title('Identity Function')
+
+    plt.subplot(2, 2, 2)
+    plt.plot(x, binary_step_output)
+    plt.title('Binary Step Function')
+
+    plt.subplot(2, 2, 3)
+    plt.plot(x, sigmoid(x))
+    plt.title('Sigmoid Function')
+
+    plt.subplot(2, 2, 4)
+    plt.plot(x, bipolar_sigmoid(x))
+    plt.title('Bipolar Sigmoid Function')
+
+    plt.tight_layout()
+    plt.show()
+
+    # ReLU is a special case and doesn't require separate subplot
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, relu(x))
+    plt.title('ReLU Function')
+    plt.show()
