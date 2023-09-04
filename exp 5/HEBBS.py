a65368a3514d07c052379a01e00f388eedb4f6af
adityaparikh7 <asparikh03@gmail.com>
2023-09-04 12:00:24 +0530

    update

--- exp 5/HEBBS.py
+++ exp 5/HEBBS.py
@@ -1,36 +1,34 @@
 import numpy as np
 
 def signum(x):
-    if x>0:
+    if x > 0:
         return 1
-    elif x==0:
+    elif x == 0:
         return 0
     else:
         return -1
-    
 
 # Define the number of neurons and the learning rate
-num_neurons = 3
+num_neurons = 4  # Update to match the size of your input patterns
 learning_rate = 0.1
 
-# Initialize the weight matrix with zeros
-weights = np.zeros((num_neurons, num_neurons))
+# Initialize the weight vector with zeros
+weights = np.array([1, -1, 0, 0.5])
 
 # Define a list of input patterns (binary vectors)
 input_patterns = [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
+    np.array([1, -2, 1.5, 0]),
+    np.array([1, -0.5, -2, -1.5]),
+    np.array([0, 1, -1, 1.5])
 ]
 
 # Iterate through input patterns and update weights using Hebb's rule
 for pattern in input_patterns:
-    pattern = np.array(pattern)
     for i in range(num_neurons):
         for j in range(num_neurons):
             if i != j:
-                weights[i][j] += learning_rate * signum(pattern[i]) * signum(pattern[j])
+                weights[i] += learning_rate * signum(pattern[i]) * signum(pattern[j])
 
-# Print the learned weight matrix
-print("Learned Weight Matrix:")
+# Print the learned weight vector
+print("Learned Weight Vector:")
 print(weights)
